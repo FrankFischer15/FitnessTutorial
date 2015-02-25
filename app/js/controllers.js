42d0f0971e91f02eda64ce757d60dbf9773372d0
Frank <Frank_Fischer_15@yahoo.com>
2015-02-25 11:55:19 -0500

    merge

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,6 +2,15 @@
 
 var fitnessTutorialControllers = angular.module('fitnessTutorialControllers', []);
 
+fitnessTutorialControllers.controller('navbarCtrl', ['$scope', '$http',
+  function($scope, $http) {
+  	$http.get('exercises/muscleGroups.json').success(function(data) {
+  	$scope.exercises = data;
+  	console.log(data)
+  	});
+  	$scope.orderProp = 'muscleGroup';
+  }]);
+
 fitnessTutorialControllers.controller('homeCtrl', ['$scope', '$http',
   function($scope, $http) {
   	$http.get('exercises/muscleGroups.json').success(function(data) {
