ee786825226517fa07ced427c70d27fd9431f6e9
Frank <Frank_Fischer_15@yahoo.com>
2015-02-25 13:32:01 -0500

    NAV almost Done

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,20 +2,23 @@
 
 var fitnessTutorialControllers = angular.module('fitnessTutorialControllers', []);
 
-fitnessTutorialControllers.controller('navbarCtrl', ['$scope', '$http',
-  function($scope, $http) {
-  	$http.get('exercises/muscleGroups.json').success(function(data) {
-  	$scope.exercises = data;
-  	console.log(data)
-  	});
-  	$scope.orderProp = 'muscleGroup';
-  }]);
+fitnessTutorialControllers.controller('navbarCtrl', ['$scope', '$http', '$routeParams',
+    function($scope, $http, $routeParams) {
+        $http.get('exercises/muscleGroups.json').success(function(data) {
+            $scope.muscleGroups = data;
+            $scope.orderProp = 'muscleGroup';
+        });
+        $http.get('exercises/exercises.json').success(function(data) {
+            $scope.exercises = data;
+            $scope.orderProp = 'name';
+            console.log(data);
+        });
+    }]);
 
 fitnessTutorialControllers.controller('homeCtrl', ['$scope', '$http',
-  function($scope, $http) {
-  	$http.get('exercises/muscleGroups.json').success(function(data) {
-  	$scope.exercises = data;
-  	console.log(data)
-  	});
-  	$scope.orderProp = 'muscleGroup';
-  }]);
+    function($scope, $http) {
+        $http.get('exercises/muscleGroups.json').success(function(data) {
+            $scope.muscleGroups = data;
+        });
+        $scope.orderProp = 'muscleGroup';
+    }]);
